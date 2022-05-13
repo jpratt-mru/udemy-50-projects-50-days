511599382ac53cf11b563b01fb3dbab8dbb8842a
jpratt-mru <jpratt@mtroyal.ca>
2022-05-13 16:08:37 -0600

    Sync

--- 01-expanding-cards/Panel.js
+++ 01-expanding-cards/Panel.js
@@ -5,28 +5,12 @@ class Panel extends HTMLElement {
 
   constructor() {
     super();
-    // const shadow = this.attachShadow({ mode: "open" });
-
-    // const div = document.createElement("div");
-    // const linkElem = document.createElement("link");
-    // linkElem.rel = "stylesheet";
-    // linkElem.href = "outstyle.css";
-    // shadow.appendChild(linkElem);
-
-    // this.textContent = this.text ? this.text : "blorp";
-    // this.appendChild(div);
   }
 
   connectedCallback() {
-    // const div = document.createElement("div");
-
-    this.className = `text-white h-[80vh] rounded-[10px] bg-cover bg-center bg-no-repeat`;
-
+    this.className = `relative transition-flex duration-50 ease-in cursor-pointer text-white h-[90vh] rounded-[30px] flex-1 bg-cover bg-center bg-no-repeat`;
     let seed = this.seed || "picsum0";
     this.style = `background-image: url('https://picsum.photos/seed/${seed}/1200/1300.webp')`;
-    //this.textContent = this.text;
-    //this.appendChild(div);
-    //this.textContent = this.text;
   }
 
   attributeChangedCallback(property, oldValue, newValue) {
